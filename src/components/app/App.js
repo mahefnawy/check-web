604cd45c0886e73a65779c3d517ac3f77f45613b
Mohamed Hassan <mohamed.hassan@osf-commerce.com>
2020-11-23 15:56:41 +0300

    FE-1000 added scss color variables

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,11 @@
 
 import './App.scss';
+import Header from '../partials/header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <h1 className="header__title--bold">this is amazing</h1>
     </div>
   );
---
+++ src/components/partials/header/Header.js
@@ -0,0 +1,12 @@
+
+import './Header.scss';
+
+function Header() {
+  return (
+    <div className="App">
+      <h1 className="header__title--yellow">this is header</h1>
+    </div>
+  );
+}
+
+export default Header;
